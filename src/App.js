40f9169a5cf31e1e947d89b3fbc3a63b11bcd6eb
Mohan <marrapumohansai@gmail.com>
2023-04-05 14:44:17 +0200

    modified button color,style in index.css

--- src/App.js
+++ src/App.js
@@ -21,16 +21,14 @@ function App ()  {
   return (
     <div>
       <center>
-        <div className="card">
-          <div className="card-body">
+        
             <h5 className="card-title">Todo Management Application</h5>
             <form onSubmit={submitHandler}>
               <input size="30" type="text" name="task" value={task} onChange={changeHandler} /> &nbsp;&nbsp;
               <input type="submit" value="Add" name="Add"/>
             </form>
             <TodoList todolist={todos} deleteHandler={deleteHandler}/>
-          </div>
-        </div>
+          
       </center>
     </div>
   )

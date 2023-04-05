17a5cf13146b16bc570da8e951e4eba2a1b782cb
Mohan <marrapumohansai@gmail.com>
2023-04-05 13:22:42 +0200

    modified App,TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,75 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-function TodoList() {
-  const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState('');
-  const [filter, setFilter] = useState('all');
-
-  const handleChange = e => {
-    setInputValue(e.target.value);
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (inputValue.trim() !== '') {
-      setTodos([...todos, { id: Date.now(), text: inputValue.trim(), completed: false }]);
-      setInputValue('');
-    }
-  };
-
-  const handleCheckbox = id => {
-    setTodos(
-      todos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, completed: !todo.completed };
-        }
-        return todo;
-      })
-    );
-  };
-
-  const handleFilter = e => {
-    setFilter(e.target.value);
-  };
-
-  const filteredTodos = todos.filter(todo => {
-    if (filter === 'active') {
-      return !todo.completed;
-    }
-    if (filter === 'completed') {
-      return todo.completed;
-    }
-    return true;
-  });
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={inputValue} onChange={handleChange} placeholder="Add a task" />
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {filteredTodos.map(todo => (
-          <li key={todo.id}>
-            <input type="checkbox" checked={todo.completed} onChange={() => handleCheckbox(todo.id)} />
-            {todo.text}
-          </li>
-        ))}
-      </ul>
-      <div>
-        <button value="all" onClick={handleFilter}>
-          All
-        </button>
-        <button value="active" onClick={handleFilter}>
-          Active
-        </button>
-        <button value="completed" onClick={handleFilter}>
-          Completed
-        </button>
-      </div>
-    </div>
-  );
+function TodoList  ({todolist,deleteHandler}) {
+    return (
+        <div>
+            {todolist.map((todo,index) =>
+            <div key={index}>
+                <h1>{todo} &nbsp; <button onClick={() => deleteHandler(index)}>Delete</button></h1>
+            </div>)}
+        </div>
+    )
 }
 
-export default TodoList;
-
+export default TodoList
